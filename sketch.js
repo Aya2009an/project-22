1e4577fa60305e24a6e36377a6fba76b9ec33935
Aya2009an <118126731+Aya2009an@users.noreply.github.com>
2023-02-22 19:20:41 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup() {
 
  // playerArcher = new ( 340, playerBase.position.y - 112, 120, 120);
   playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
-  //playerArcher =  PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+ // playerArcher =  PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
  // playerArcher = new PlayerArcher( );
 
 }
